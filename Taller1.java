c4b78b12799e6e9d9b3f4b8766d7a0e34c11deec
Marcelo <m.henriquez11@ufromail.cl>
2022-03-23 09:06:17 -0300

    Segundo commit

---
+++ Taller1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+
+// Hacer un arreglo para definir la cantidad de celdas del carrito.
+
+public class Taller1 {
+    public static void main(String[] args) {
+        int celdas=0;
+        boolean carrito[] = new boolean[celdas];
+
+    }
+
+    public static boolean carrito (celdas ) {
+
+    }
+}
